88154327ebd623386bc5882a07f6e26a28d5d768
PedroAmLemos <peedroamlemos@gmail.com>
2021-09-16 12:11:25 -0300

    prj combustivel

---
+++ PrjCombustivel/src/br/uel/combustivel/Bomba.java
@@ -0,0 +1,65 @@
+package br.uel.combustivel;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Bomba {
+	private int numeroId;
+	private ArrayList<Combustivel> combustiveis = new ArrayList<>();
+
+	public int getNumeroId() {
+		return numeroId;
+	}
+
+	public void setNumeroId(int numeroId) {
+		this.numeroId = numeroId;
+	}
+	
+	public void addCombustivel(String nome, double preco) {
+		Combustivel novoCombustivel = new Combustivel(nome, preco);
+		this.combustiveis.add(novoCombustivel);
+	}
+	
+	public void selectCombustivel(String nome, Scanner input) {
+		int opcao = 0;
+		double qntLitro;
+		double qntDinheiro;
+		int flag = 0;
+		for(Combustivel combustivel : combustiveis) {
+			if(nome.equals(combustivel.getNomeCombustivel())) {
+				flag = 1;
+				do {
+					System.out.println("O preço do combustível "+ combustivel.getNomeCombustivel()+ " é "+ combustivel.getPreco());
+					System.out.println("Escolha uma opção: ");
+					System.out.println("1) Inserir por litro");
+					System.out.println("2) Inserir por dinheiro");
+					System.out.println("3) Sair");
+					opcao = input.nextInt();
+					input.nextLine();
+					switch(opcao) {
+						case 1:
+							System.out.println("Insira a quantidade de litros: ");
+							qntLitro = Double.valueOf(input.nextLine());
+							combustivel.absLitro(qntLitro);
+							System.out.println("Digite (3) para voltar ou (0) para abastecer novamente");
+							opcao = Integer.valueOf(input.nextLine());
+							break;
+						case 2:
+							System.out.println("Insira a quantidade de dinheiro: ");
+							qntDinheiro = Double.valueOf(input.nextLine());
+							combustivel.absDin(qntDinheiro);
+							System.out.println("Digite (3) para voltar ou (0) para abastecer novamente");
+							opcao = Integer.valueOf(input.nextLine());
+							break;
+						case 3:
+							break;
+					}
+				} while(opcao != 3);
+				break;
+			}
+		}
+		if(flag == 0) {
+			System.out.println("O combustivel " +nome +" não foi encontrada");
+		}
+	}
+
+}
---
+++ PrjCombustivel/src/br/uel/combustivel/Combustivel.java
@@ -0,0 +1,40 @@
+package br.uel.combustivel;
+
+public class Combustivel {
+	private String nomeCombustivel;
+	private double preco;
+
+	public Combustivel(String nome, double preco) {
+		this.nomeCombustivel = nome;
+		this.preco = preco;
+	}
+
+	public String getNomeCombustivel() {
+		return nomeCombustivel;
+	}
+
+	public void setNomeCombustivel(String nomeCombustivel) {
+		this.nomeCombustivel = nomeCombustivel;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	public double absLitro(double qntLitro) {
+		double preco = qntLitro * this.preco;
+		System.out.println("O preco de "+ qntLitro+ " litros da gasolina "+ this.nomeCombustivel +" é "+preco +" reais");
+		return preco;
+	}
+	
+	public double absDin(double qntDin) {
+		double qntLitro = qntDin / preco;
+		System.out.println("Foram abastecidos "+ qntLitro +" litros " + "com " +qntDin +" reais, a gasolina utilizada foi a " +this.nomeCombustivel);
+		return qntLitro;
+	}
+
+}
